7930f26ee9070953e299f4d6c15e36ca9f2bf151
Savyasachi <jsavyasachi@gmail.com>
2026-06-22 22:23:05 -0700

    build: scaffold lein project (EPL-1.0, DJL tokenizers 0.33.0)

---
+++ project.clj
@@ -0,0 +1,19 @@
+(defproject net.clojars.savya/tokenizers-clj "0.1.0"
+  :description "Idiomatic Clojure wrapper over DJL's HuggingFace tokenizers (native Rust tokenizers via JNI): encode, decode, and count tokens."
+  :url "https://github.com/jsavyasachi/tokenizers-clj"
+  :license {:name "Eclipse Public License 1.0"
+            :url "https://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git" :url "https://github.com/jsavyasachi/tokenizers-clj"}
+  :dependencies [[ai.djl.huggingface/tokenizers "0.33.0"]]
+  :global-vars {*warn-on-reflection* true}
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.12.0"]]}
+             :dev {:dependencies [[org.clojure/clojure "1.12.0"]]
+                   :resource-paths ["test/resources"]}
+             :clojure-1-10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
+             :clojure-1-11 {:dependencies [[org.clojure/clojure "1.11.4"]]}
+             :clojure-1-12 {:dependencies [[org.clojure/clojure "1.12.0"]]}}
+  :aliases {"all" ["with-profile" "+clojure-1-10:+clojure-1-11:+clojure-1-12"]}
+  :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
+                                    :username :env/clojars_username
+                                    :password :env/clojars_password
+                                    :sign-releases false}]])
